157b7658d97d69aa300c4d0f247dc9ae1cececa8
ImJustRob <robertalothian@hotmail.com>
2024-11-20 19:45:01 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { PortfoliosPage } from "./PortfoliosPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <PortfoliosPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
